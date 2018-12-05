70d7ce8d0d369bfca4e9372516cb6a71edf1207a
Johannes Elmnäinen <sanhologin@gmail.com>
2018-12-06 00:41:45 +0200

    remove unnecessary imports

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { colors, margins } from './styles'
+import { colors } from './styles'
 
 const headerWrapperStyle = {
   background: colors.primary,
