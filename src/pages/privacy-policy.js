ec384409abf52e8df5fa8b849e4ec178ac086d4f
Johannes Elmnäinen <sanhologin@gmail.com>
2019-01-03 00:06:18 +0200

    Add data usage explanation to privacy policy

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -21,9 +21,14 @@ const SecondPage = () => (
       <h3>3rd parties</h3>
       The data will not be shared with any 3rd parties.
 
-      <h3>Data usage / persistance</h3>
+      <h3>Data usage</h3>
+      The data will be used to keep a list of persons applied to the event. It will also be used to contact the applicants
+      through their email.
+
+      <h3>Data persistance</h3>
       The data will be used until the event registered for has been held, or 6 months after input (whichever comes first).
       The data will then be destroyed.
+
 
       <h3>Rights of the data subject</h3>
       Applicants have right to get, modify or delete their data by informing reacTour at reactour.org@gmail.com.
